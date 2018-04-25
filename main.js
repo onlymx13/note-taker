54b10847a1942b8fd9d9f38ca26cb4c2b8871d60
onlymx13 <onlymx13@gmail.com>
2018-04-24 19:59:05 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function takeNotes() {
+var keyWords = document.getElementById('keywords').value.split(" ");
+var sourceText = document.getElementById('textarea').value.split(" ");
+for (var i=0;i<sourceText.length;i++){
+    if(/\d/.test(sourceText[i]) || keyWords.indexOf(sourceText[i]) != -1) {
+        var x = i;
+        do {
+        x--;
+        } while (x>0 && sourceText[x].indexOf('.') == -1);
+        var str=sourceText[x];
+        for (var j=x+1;j<=i;j++) {
+        str += " "+sourceText[j];
+        }
+        var node=document.createTextNode(str);
+        var list=document.getElementById('list')
+        list.appendChild(node);
+    }
+}
+}
