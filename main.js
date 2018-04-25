798672f9299dc126b0c06ac58207075c588d16ec
onlymx13 <onlymx13@gmail.com>
2018-04-24 20:02:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ for (var i=0;i<sourceText.length;i++){
     if(/\d/.test(sourceText[i]) || keyWords.indexOf(sourceText[i]) != -1) {
         var x = i;
         do {
-        x--;
+            x--;
         } while (x>0 && sourceText[x].indexOf('.') == -1);
         var str=sourceText[x];
         for (var j=x+1;j<=i;j++) {
-        str += " "+sourceText[j];
+            str += " "+sourceText[j];
         }
-        var node=document.createTextNode(str);
+        var node=document.createTextNode(str+"\n");
         var list=document.getElementById('list')
         list.appendChild(node);
     }
