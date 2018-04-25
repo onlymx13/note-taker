fcaec68d881c3225d05c29b41f63f45d501857f1
onlymx13 <onlymx13@gmail.com>
2018-04-25 09:33:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ sourceSentences = document.getElementById('textarea').value.split(/.!?/);
 for (var i=0;i<sourceSentences.length;i++) {
     sourceText = sourceSentences[i].split(" ");
     sourceText.forEach(function(element){
-        if (sourceText.test(/[0-9]/) || sourceText.indexOf(element)) {
+        if (/[0-9]/.test(sourceText) || sourceText.indexOf(element) != -1) {
             var p = document.createElement('p');
             p.innerHTML = sourceSentences[i];
             document.body.insertBefore(p,null);
