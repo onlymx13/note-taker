cd643393764d020836e7c214164e4d3ffa003234
onlymx13 <onlymx13@gmail.com>
2018-04-25 09:52:37 -0500

    Complete revamp again

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ var keyWords;
 var sourceSentences;
 var sourceText;
 function takeNotes() {
-keyWords = document.getElementById('keywords').value.split(" ");
-sourceSentences = document.getElementById('textarea').value.split(/.!?/);
-for (var i=0;i<sourceSentences.length;i++) {
-    sourceText = sourceSentences[i].split(" ");
-    sourceText.forEach(function(element){
-        if (/[0-9]/.test(sourceText) || sourceText.indexOf(element) != -1) {
-            var p = document.createElement('p');
-            p.innerHTML = sourceSentences[i];
-            document.body.insertBefore(p,null);
+    keyWords = document.getElementById('keywords').value.split(" ");
+    sourceSentences = document.getElementById('textarea').value.split(/.!?/);
+    for (let sentence of sourceSentences) {
+        sourceText = sentence.split(" ");
+        if (/[0-9]/.test(sentence)) {
+            add(sentence);    
         }
-    });
+        for (let keyWord of keyWords) {
+            if (sourceText.indexOf(keyWord) != -1) {
+                add(sentence);
+            }
+        }
+    }
 }
+function add(str) {
+    console.log(str);
+    var p = document.createElement('p');
+    p.innerHTML = str;
+    document.insertBefore(p,null);
 }
